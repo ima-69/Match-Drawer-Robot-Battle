bb2850e623980c7add8683929e45b6d9fb6e1f49
ima-69 <imansha.idr@gmail.com>
2025-09-16 23:06:43 +0530

    created folder structure and base code

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// server/server.js
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Basic test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
